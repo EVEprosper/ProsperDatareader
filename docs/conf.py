b4ed26844d1ea3c0c336cc7e77e88a5514652085
Lockefox <locke.renard@gmail.com>
2017-08-12 22:28:17 -0700

    updating docs to match prospercommon

--- docs/conf.py
+++ docs/conf.py
@@ -110,8 +110,9 @@ html_theme_options = {
     'github_user': 'eveprosper',
     'github_repo': 'prosperdatareader',
     'github_banner': True,
-    #'donate_url': 'https://www.patreon.com/eveprosper',
 }
+
+html_favicon = "static/prosper.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
