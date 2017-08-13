fe689d62ec64dd360c52790738b6f1220a31b9a4
Lockefox <locke.renard@gmail.com>
2017-08-12 20:33:03 -0700

    removing import from conf.py
    TODO: find alternate autodocs solution

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,12 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../prosper'))
+
+## vv TODO vv: autodocs ##
+#import os
+#import sys
+#sys.path.insert(0, os.path.abspath('../prosper'))
+## ^^ TODO ^^ ##
 import alabaster
 
 # -- General configuration ------------------------------------------------
