09bf10d703ba0a1a61cc7b7eca0f08de06c3c1dc
lockefox <locke.renard@gmail.com>
2017-12-31 12:33:38 -0800

    adding news endpoint for intrinio

--- prosper/datareader/news.py
+++ prosper/datareader/news.py
@@ -3,6 +3,7 @@
 import pandas as pd
 
 import prosper.datareader.robinhood as robinhood  # TODO: simplify import
+import prosper.datareader.intrinino as intrinino
 import prosper.datareader.exceptions as exceptions
 import prosper.datareader.config as config
 
@@ -32,6 +33,53 @@ def company_news_rh(
     logger.info('--Pushing data into Pandas')
     news_df = pd.DataFrame(raw_news_data)
     news_df['published_at'] = pd.to_datetime(news_df['published_at'])
+
+    logger.debug(news_df)
+    return news_df
+
+def company_news_intrinino(
+        ticker,
+        username='',
+        password='',
+        public_key='',
+        endpoint_name='news',
+        logger=config.LOGGER,
+):
+    """get news items from Intrinino
+
+    Notes:
+        credentials required from: https://intrinio.com/account
+        username/password OR public_key, not both
+
+    Args:
+        ticker (str): stock ticker for desired company
+        username (str): intrinio username
+        password (str): intrinio password
+        public_key (str): intrinio public_key
+        logger (:obj:`logging.logger`): logging handle
+
+    Returns:
+        pandas.DataFrame: tabularized data for news
+
+    Raises:
+        exceptions.InvalidAuth: invalid auth pattern
+        requests.exceptions: HTTP/connection errors
+
+    """
+    logger.info('Fetching company raw data feed for `%s` -- INTRININO', ticker)
+    connection = intrinino.auth.IntrininoHelper(
+        username=username,
+        password=password,
+        public_key=public_key,
+    )
+    raw_data = connection.request(
+        endpoint_name,
+        params={'ticker': ticker.upper()}
+    )
+
+    logger.info('--pushing data into Pandas')
+    news_df = pd.DataFrame(raw_data['data'])
+    news_df['publication_date'] = pd.to_datetime(news_df['publication_date'])
 
     logger.debug(news_df)
     return news_df
